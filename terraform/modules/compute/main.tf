eb8a56471fd582b9f8955798b6e76dbe85c463b2
Pragash Kumaravel <pragash_m@hotmail.co.uk>
2026-08-31 18:52:13 +0200

    Add Route53 alias record pointing domain to the ALB

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -206,6 +206,19 @@ resource "aws_lb_listener" "https" {
   }
 }
 
+resource "aws_route53_record" "app" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.main.dns_name
+    zone_id                = aws_lb.main.zone_id
+    evaluate_target_health = true
+  }
+}
+
+
 # --- Launch template ---
 
 resource "aws_launch_template" "app" {
