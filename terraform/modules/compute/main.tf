eeca420674f65fd30cd93c12a2aeff8ac9a155c4
Pragash Kumaravel <pragash_m@hotmail.co.uk>
2026-08-31 18:47:14 +0200

    Add ACM DNS validation records and certificate validation

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -131,6 +131,28 @@ resource "aws_acm_certificate" "main" {
   }
 }
 
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id = aws_route53_zone.main.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "main" {
+  certificate_arn         = aws_acm_certificate.main.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
+
+
 
 resource "aws_lb_target_group" "app" {
   name_prefix = "ckit-"
