f967f03167369803357a982a807d429a615ec70d
Pragash Kumaravel <pragash_m@hotmail.co.uk>
2026-09-02 17:33:52 +0200

    Revert business signal widgets to line view per final preference

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -194,7 +194,6 @@ resource "aws_cloudwatch_dashboard" "main" {
         width  = 12
         height = 6
         properties = {
-          view   = "bar",
           title  = "Orders Placed"
           region = "eu-west-3"
           metrics = [
@@ -209,7 +208,6 @@ resource "aws_cloudwatch_dashboard" "main" {
         width  = 12
         height = 6
         properties = {
-          view   = "bar",
           title  = "Revenue (EUR)"
           region = "eu-west-3"
           metrics = [
